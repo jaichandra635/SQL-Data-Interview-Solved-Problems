0b5740eef300bf621dd8c528b6e6b8715e6e23f6
JAI CHANDRA <97591346+jaichandra635@users.noreply.github.com>
2026-02-18 23:26:04 +0530

    Implement SQL query for third transaction per user
    Added SQL query to retrieve the third transaction for each user, including user_id, spend, and transaction_date.

--- users_third_transaction.sql
+++ users_third_transaction.sql
@@ -1,30 +1,20 @@
 # 🚗 Uber SQL Interview Question – Third Transaction Per User - Medium
-
 ## 📌 Problem Statement
-
 Assume you are given a table of Uber transactions made by users.
-
 Write a SQL query to obtain the **third transaction of every user**.
-
 Return the following columns:
 - `user_id`
 - `spend`
 - `transaction_date`
-
 ---
-
 ## 🗂 Table: transactions
-
 | Column Name       | Type      |
 |------------------|----------|
 | user_id           | integer  |
 | spend             | decimal  |
 | transaction_date  | timestamp |
-
 ---
-
 ## 📥 Example Input
-
 | user_id | spend  | transaction_date       |
 |----------|--------|------------------------|
 | 111      | 100.50 | 01/08/2022 12:00:00    |
@@ -32,23 +22,13 @@ Return the following columns:
 | 121      | 36.00  | 01/18/2022 12:00:00    |
 | 145      | 24.99  | 01/26/2022 12:00:00    |
 | 111      | 89.60  | 02/05/2022 12:00:00    |
-
 ---
-
 ## 📤 Expected Output
-
 | user_id | spend | transaction_date       |
 |----------|--------|------------------------|
 | 111      | 89.60 | 02/05/2022 12:00:00    |
 
----
-
-## 📝 Notes
-
-- Transactions should be ordered by `transaction_date`.
-- Only return users who have at least **three transactions**.
-
-
+  
 ## My Solution
 SELECT
 t.user_id,
@@ -61,3 +41,11 @@ SELECT
   row_number() over(partition by user_id order by transaction_date) as rnk
 FROM transactions)t
 where rnk = 3;
+
+
+## Output
+user_id	spend	transaction_date
+111	90	2022-02-05 12:00:00
+121	68	2022-04-03 12:00:00
+263	100	2022-07-12 12:00:00
+
